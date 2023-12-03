0f31232424e39a2811c49c2bd7837c873376bf43
MehakK1311 <mehakhanna1311@gmail.com>
2023-12-03 17:46:01 +0530

    connecting socket

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const path = require('path')
+const io = require('socket.io')(http)
+
+app.get('/', (req,res)=>{
+    const options={
+        root: path.join(__dirname)
+    }
+    const fileName = 'index.html'
+    res.sendFile(fileName, options)
+})
+
+io.on('connection', (socket)=>{
+    console.log('user connected')
+
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected')
+    })
+});
+
+http.listen(3000, ()=>{
+    console.log('app running')
+})
